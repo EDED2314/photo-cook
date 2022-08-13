a0a748f741eeb1ed774a4226011bbaa8ee675c43
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-08-13 01:06:53 -0400

    base ui done and func too

--- flutter/photo_cook/lib/screens/cam.dart
+++ flutter/photo_cook/lib/screens/cam.dart
@@ -2,6 +2,7 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:photo_cook/widgets/coolButtion.dart';
+import 'package:photo_cook/widgets/coolText.dart';
 
 class CameraApp extends StatefulWidget {
   /// Default Constructor
@@ -13,35 +14,81 @@ class CameraApp extends StatefulWidget {
 
 class _CameraAppState extends State<CameraApp> {
   XFile? imageFile;
+  bool picked = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Column(
-        children: [
-          const Spacer(),
-          ExpandedButton(
-              onPressed: () async {
-                try {
-                  final pickedImage =
-                      await ImagePicker().pickImage(source: ImageSource.camera);
-                  if (pickedImage != null) {
-                    imageFile = pickedImage;
-                    setState(() {});
+    if (!picked) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const coolText(
+            text: "Cooker",
+            fontSize: 14,
+          ),
+        ),
+        body: Center(
+            child: Column(
+          children: [
+            const Spacer(),
+            ExpandedButton(
+                onPressed: () async {
+                  try {
+                    final pickedImage = await ImagePicker()
+                        .pickImage(source: ImageSource.camera);
+                    if (pickedImage != null) {
+                      setState(() {
+                        imageFile = pickedImage;
+                        picked = true;
+                      });
+                    }
+                  } catch (e) {
+                    imageFile = null;
+                    if (mounted) {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: coolText(
+                          text: e.toString(),
+                          fontSize: 5,
+                        ),
+                      ));
+                    }
                   }
-                } catch (e) {
-                  imageFile = null;
-                  setState(() {});
-                }
-              },
-              text: "text",
-              flex: 2,
-              fontSize: 12,
-              width: 200),
-          const Spacer(),
-        ],
-      )),
-    );
+                },
+                text: "Take Photo",
+                flex: 2,
+                fontSize: 15,
+                width: 200),
+            const Spacer(),
+          ],
+        )),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: const coolText(
+            text: "Cooker",
+            fontSize: 14,
+          ),
+        ),
+        body: Center(
+            child: Column(
+          children: [
+            const Spacer(),
+            const coolText(text: "Data", fontSize: 15),
+            const Spacer(),
+            ExpandedButton(
+                onPressed: () {
+                  setState(() {
+                    picked = false;
+                  });
+                },
+                text: "Take Photo Again",
+                flex: 1,
+                fontSize: 12,
+                width: 150),
+            const Spacer(),
+          ],
+        )),
+      );
+    }
   }
 }
