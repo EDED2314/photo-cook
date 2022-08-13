e6350cf0e74fccd3dec05d94b5f8677caa633b72
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-08-12 23:10:15 -0400

    homepage done

--- flutter/photo_cook/lib/screens/home_page.dart
+++ flutter/photo_cook/lib/screens/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/coolText.dart';
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key,}) : super(key: key);
 
@@ -18,29 +20,46 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
-  Widget build(BuildContext context) {
+ Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Photo Cook"),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            const Spacer(
+              flex: 22,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            const SizedBox(
+              width: 300,
+              child: coolText(
+                text: 'Welcome to Photo Cook!',
+                fontSize: 25,
+              ),
             ),
+            const SizedBox(
+              height: 20,
+            ),
+            CircleAvatar(
+              radius: 80,
+              backgroundColor: Colors.blue,
+              child: Padding(
+                padding: const EdgeInsets.all(12), // Border radius
+                child: ClipOval(
+                  child: Image.asset(
+                    "lib/assets/images.png",
+                  ),
+                ),
+              ),
+            ),
+            const Spacer(
+              flex: 40,
+            ),
+            const coolText(
+                text: "click one of the buttons below to get started",
+                fontSize: 8),
+            const Spacer(),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
