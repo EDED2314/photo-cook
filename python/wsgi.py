28bebb86512892c828428e0054329c1c2c4a4fd7
EDED2314 <eddietang2314@gmail.com>
2022-08-13 18:58:49 -0400

    hackathon

--- python/wsgi.py
+++ python/wsgi.py
@@ -6,27 +6,32 @@ from detect.predict import get_detections
 app = Flask(__name__)
 app.config["SECRET_KEY"] = f"{os.urandom(24).hex()}"
 
+global counter
+counter = 0
+
+
 @app.route('/', methods=["GET"])
 def home():
     return render_template("index.html")
 
 @app.route('/api/cook', methods=["GET", 'POST'])
 def process_image_get_detections():
+    global counter
+    counter += 1
     images = request.files.getlist("images")
-    if len(images) == 0:
+    if len(images) == 0 and counter % 2 == 1:
+        print("POST")
         return jsonify({"error": "400", "message": "Please make sure you are sending an image with the request!"})
     if request.method == "POST":
         for image in images:
             amount_of_images = len(os.listdir("Images"))
             image.save(os.path.join("Images", f"{amount_of_images + 1}.jpg"))
+            return ""
     elif request.method == "GET":
-        response = {}
-        for image in images:
-            amount_of_images = len(os.listdir("Images"))
-            image.save(os.path.join("Images", f"{amount_of_images+1}.jpg"))
-            class_labels = get_detections(os.path.join("Images", f"{amount_of_images+1}.jpg"))
-            response = send(class_labels)
-            response['detections'] = class_labels
+        amount_of_images = len(os.listdir("Images"))
+        class_labels = get_detections(os.path.join("Images", f"{amount_of_images}.jpg"))
+        response = send(class_labels)
+        response['detections'] = class_labels
         return jsonify(response)
 
 if __name__ == '__main__':
