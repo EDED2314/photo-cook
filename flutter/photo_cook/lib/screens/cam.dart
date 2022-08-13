cd1f8d84b69ae3ce5b58374c43bb7c5967ae6aab
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-08-13 18:59:11 -0400

    cool code

--- flutter/photo_cook/lib/screens/cam.dart
+++ flutter/photo_cook/lib/screens/cam.dart
@@ -6,7 +6,22 @@ import 'package:photo_cook/widgets/coolButtion.dart';
 import 'package:photo_cook/widgets/coolText.dart';
 import 'package:http/http.dart' as http;
 
-import 'http.dart';
+class Data {
+  final List detections;
+  final List results;
+
+  const Data({
+    required this.detections,
+    required this.results,
+  });
+
+  factory Data.fromJson(Map<String, dynamic> json) {
+    return Data(
+      detections: json['detections'],
+      results: json['results'],
+    );
+  }
+}
 
 class CameraApp extends StatefulWidget {
   /// Default Constructor
@@ -17,6 +32,42 @@ class CameraApp extends StatefulWidget {
 }
 
 class _CameraAppState extends State<CameraApp> {
+  Future<void> sendData() async {
+    try {
+      final pickedImage =
+          await ImagePicker().pickImage(source: ImageSource.camera);
+      if (pickedImage != null) {
+        setState(() {
+          imageFile = pickedImage;
+          picked = true;
+        });
+      }
+      http.MultipartRequest request = http.MultipartRequest(
+          'POST', Uri.parse("http://192.168.0.133:5000/api/cook"));
+
+      request.files.add(
+        await http.MultipartFile.fromPath(
+          'images',
+          imageFile!.path,
+        ),
+      );
+      http.StreamedResponse r = await request.send();
+      final response =
+          await http.get(Uri.parse('http://192.168.0.133:5000/api/cook'));
+      cool = Data.fromJson(jsonDecode(response.body));
+    } catch (e) {
+      imageFile = null;
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: coolText(
+            text: e.toString(),
+            fontSize: 5,
+          ),
+        ));
+      }
+    }
+  }
+
   XFile? imageFile;
   bool picked = false;
   Data cool = const Data(detections: ["Loading"], results: ["Loading"]);
@@ -35,41 +86,8 @@ class _CameraAppState extends State<CameraApp> {
           children: [
             const Spacer(),
             ExpandedButton(
-                onPressed: () async {
-                  try {
-                    final pickedImage = await ImagePicker()
-                        .pickImage(source: ImageSource.camera);
-                    if (pickedImage != null) {
-                      setState(() {
-                        imageFile = pickedImage;
-                        picked = true;
-                      });
-                    }
-                    http.MultipartRequest request = http.MultipartRequest(
-                        'POST',
-                        Uri.parse("http://192.168.0.133:5000/api/cook"));
-
-                    request.files.add(
-                      await http.MultipartFile.fromPath(
-                        'images',
-                        imageFile!.path,
-                      ),
-                    );
-                    http.StreamedResponse r = await request.send();
-                    final response = await http
-                        .get(Uri.parse('http://192.168.0.133:5000/api/cook'));
-                    cool = Data.fromJson(jsonDecode(response.body));
-                  } catch (e) {
-                    imageFile = null;
-                    if (mounted) {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: coolText(
-                          text: e.toString(),
-                          fontSize: 5,
-                        ),
-                      ));
-                    }
-                  }
+                onPressed: () {
+                  sendData();
                 },
                 text: "Take Photo",
                 flex: 2,
